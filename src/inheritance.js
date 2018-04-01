1a8af6c5324871d949d1334ab6426ad14e3fe260
David Mutua <mutuadavi@gmail.com>
2018-04-01 14:38:15 +0200

    Demonstrated inheriting Contructors

--- src/inheritance.js
+++ src/inheritance.js
@@ -3,6 +3,13 @@
 // @Parent Class. Holds all Common/General Information.
 class Vehicle {
     
+    // Base Constructor.
+    // NB: Always Called First when an Instance is Created.
+    constructor(licenseNumber) {
+        this.licenseNumber = licenseNumber;
+        console.log("Vehicle constructor");
+    }
+    
 }
 
 // @Child class. Holds Specific Information About itself.
@@ -14,12 +21,24 @@ class Drone extends Vehicle {
 // @Child class. Holds Specific Information About itself.
 class Car extends Vehicle {
     
+    // Inheriting Constructors.
+    // You MUST use Super First to call Parent Constructor.
+    constructor(licenseNumber) {
+        
+        // Access a Licence Number Stored in a Parent Constructor.
+        super(licenseNumber); // call Parent constructor First!!
+        console.log("Car constructor");
+    }
+    
 };
 
 
 // Instantiate a Car.
-let c = new Car();
+let c = new Car("BV567");
 
 // Now c is an Instance of All the Parents of Car Class;
 // i.e. Vehicle and Object.
-console.log(c instanceof Object);
+//console.log(c instanceof Object);
+
+
+console.log(c.licenseNumber); // BV567
