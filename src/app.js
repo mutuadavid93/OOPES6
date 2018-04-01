54184034032bef0552accb09b0770f190d70d154
David Mutua <mutuadavi@gmail.com>
2018-04-01 14:00:45 +0200

    Created Getters and Setters

--- src/app.js
+++ src/app.js
@@ -33,7 +33,9 @@ class Drone {
     constructor(id, name) {
         // "this" refers to the Instance being Created e.g. drone
         // NB: in this.id, the id is an instance property.
-        this.id = id;
+        // NB: using _propertyName, indicates it's private, use a Getter to 
+        // expose it publicly.
+        this._id = id;
         this.name = name;
     }
     
@@ -56,6 +58,18 @@ class Drone {
         // Use "this" to invoke other Methods on that Class Instance too.
         console.log(`Drone ${this.id} ${this.moveRotors()}`);
     }
+    
+    // Getters in a Class.
+    // NB: Getters are Properties. Thus accessed without the brackets.
+    get id() {
+        // Now Expose the private instance property _id.
+        return this._id;
+    }
+    
+    // Setters in a Class
+    set id(value) {
+        this._id = value;
+    }
 }
 
 // Add a Static Property( e.g. maxHeight ) directly into the Drone Class;
@@ -66,6 +80,9 @@ let drone = new Drone("A347", "Flyer");
 
 console.log(typeof drone); // Object
 console.log(drone instanceof Drone); // true
+
+// NB: A Setter is invoked when a value is Assigned to the instance property.
+drone.id = "P456"; // now id changes from A347 to P456.
 
 console.log(`drone: ${drone.id} ${drone.name}`);
 
